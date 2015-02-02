17b4bc5becc5a05ba2cf622d8cef6a0a0448f019
loint <loint@penlook.com>
2015-02-02 22:47:04 +0700

    Fixed copyright by penlook development team

--- lib/src/sample_test.dart
+++ lib/src/sample_test.dart
@@ -1,5 +1,9 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+/**
+ * Copyright 2014 Penlook Development Team. All rights reserved.
+ * Use of this source code is governed by
+ * license that can be found in the LICENSE file.
+ * Author : Loi Nguyen <loint@penlook.com>
+ */
 
 library penlook.test;
 
