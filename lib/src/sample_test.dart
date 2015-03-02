a34c1ee63f84a4d816ff2cb6d76975bb58878eb9
loint <loint@penlook.com>
2015-03-03 03:48:30 +0700

    Add new version for frontend

---
+++ lib/src/sample_test.dart
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2014 Penlook Development Team. All rights reserved.
+ * Use of this source code is governed by
+ * license that can be found in the LICENSE file.
+ * Author : Loi Nguyen <loint@penlook.com>
+ */
+
+library frontend.test;
+
+import "package:unittest/unittest.dart";
+import "package:frontend/frontend.dart";
+
+main() {
+
+    group('SAMPLE TEST GROUP', () {
+
+        Person boy;
+
+        setUp(() {
+            boy = new Person("NamVH");
+            boy.setAge(20);
+        });
+
+        test('TEST 1', () {
+            expect(boy.getName(), "NamVH");
+            expect(boy.getAge(), 20);
+        });
+  });
+
+}
